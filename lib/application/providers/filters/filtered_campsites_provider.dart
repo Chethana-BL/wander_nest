530b5a30d194848719c8c1daf65617c907c1427e
Chethana-BL <chethanabl27@gmail.com>
2025-06-28 10:54:15 +0200

    fix: build errors and refactored filtering logic

--- lib/application/providers/filters/filtered_campsites_provider.dart
+++ lib/application/providers/filters/filtered_campsites_provider.dart
@@ -5,8 +5,8 @@ import 'package:wander_nest/data/models/campsite.dart';
 import 'campsite_filter_notifier.dart';
 
 /// [filteredCampsitesProvider] filters the list of campsites based on the current filter settings.
-/// It listens to the `campsiteListProvider` for the full list of campsites
-/// and the `campsiteFilterProvider` for the current filter settings.
+/// It listens to the [campsiteListProvider] for the full list of campsites
+/// and the [campsiteFilterProvider] for the current filter settings.
 final filteredCampsitesProvider = Provider<List<Campsite>>((ref) {
   final allCampsites = ref
       .watch(campsiteListProvider)
@@ -15,21 +15,26 @@ final filteredCampsitesProvider = Provider<List<Campsite>>((ref) {
 
   return allCampsites.where((c) {
     // Handle Close to Water true/false
-    if (filter.isCloseToWater == true && !c.isCloseToWater) return false;
-    if (filter.isCloseToWater == false && c.isCloseToWater) return false;
+    if (filter.isCloseToWater != null &&
+        filter.isCloseToWater != c.isCloseToWater) {
+      return false;
+    }
 
     // Handle Campfire Allowed true/false
-    if (filter.isCampFireAllowed == true && !c.isCampFireAllowed) return false;
-    if (filter.isCampFireAllowed == false && c.isCampFireAllowed) return false;
+    if (filter.isCampFireAllowed != null &&
+        filter.isCampFireAllowed != c.isCampFireAllowed) {
+      return false;
+    }
 
     // Host languages
     if (filter.hostLanguages != null && filter.hostLanguages!.isNotEmpty) {
-      final matchesLanguage = c.hostLanguages.any(
-        (lang) => filter.hostLanguages!.any(
-          (fLang) => fLang.toLowerCase() == lang.toLowerCase(),
-        ),
-      );
-      if (!matchesLanguage) return false;
+      if (!c.hostLanguages.any(
+        (lang) => filter.hostLanguages!
+            .map((f) => f.toLowerCase())
+            .contains(lang.toLowerCase()),
+      )) {
+        return false;
+      }
     }
 
     // Price range
