925211cd3d301cd6e90ea06b94c0627d9bde6722
Chethana-BL <chethanabl27@gmail.com>
2025-06-30 20:40:46 +0200

    test(api): add unit tests for CampsiteApiService
    - Implemented mock API service for testing
    - Updated dependencies to support testing framework

--- lib/data/datasources/remote/campsite_api_service.dart
+++ lib/data/datasources/remote/campsite_api_service.dart
@@ -4,12 +4,14 @@ import 'package:http/http.dart' as http;
 import 'package:wander_nest/data/models/campsite.dart';
 
 class CampsiteApiService {
+  CampsiteApiService({http.Client? client}) : client = client ?? http.Client();
   static const _baseUrl =
       'https://62ed0389a785760e67622eb2.mockapi.io/spots/v1';
+  final http.Client client;
 
   Future<List<Campsite>> fetchCampsites() async {
     try {
-      final response = await http.get(Uri.parse('$_baseUrl/campsites'));
+      final response = await client.get(Uri.parse('$_baseUrl/campsites'));
 
       if (response.statusCode == 200) {
         final List<dynamic> data = json.decode(response.body);
